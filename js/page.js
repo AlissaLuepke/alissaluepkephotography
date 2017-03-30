e0629d3976b36231b4ed07700bbbd490117bfdb4
Alissa <alissa@luepke.us>
2017-03-30 10:59:53 +0200

    webseite

---
+++ js/page.js
@@ -0,0 +1,42 @@
+var curImg = 1;
+var timestamp = 0;
+
+$(document).ready(function () {
+
+ $('.top').UItoTop();
+    
+    $(".nav a").on("click", function () {
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+
+
+
+    
+    
+   
+ 
+
+
+
+});
+
+var timer = setInterval(changePic, 5000);
+var timer2 = setInterval(updateBalken, 1);
+
+function changePic() {
+    var newLeft = (curImg % 4 * 100);
+    console.log("triggered");
+    $('.header-gallery').animate({
+        right: newLeft + '%'
+    }, 900);
+    curImg++;
+    timestamp = 0;
+}
+
+function updateBalken() {
+    timestamp++;
+    $('.balken-gallery').animate({
+        width: timestamp / (50 / 4) + "%"
+    }, 0);
+}
